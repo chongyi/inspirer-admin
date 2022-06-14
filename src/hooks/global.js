b9570ee1779a3ff36b69af270d5a3f80ec89a95f
Chongyi <xpz3847878@163.com>
2022-06-14 23:31:00 +0800

    布局样式

---
+++ src/hooks/global.js
@@ -0,0 +1,12 @@
+export function useHeaderMenus() {
+    return [
+        {
+            key: 'content',
+            label: '内容中心'
+        },
+        {
+            key: 'system',
+            label: '系统'
+        },
+    ]
+}
