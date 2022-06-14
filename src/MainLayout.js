1f6e43024191df4c94827a9927dfdc53758f3a84
Chongyi <xpz3847878@163.com>
2022-06-14 23:31:00 +0800

    布局样式

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,12 +1,29 @@
 import { Outlet } from 'react-router-dom'
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+import { useHeaderMenus } from './hooks/global';
+
+const { Header, Content, Sider } = Layout;
 
 const MainLayout = () => {
-    return <div>
-        <h1>测试框架</h1>
-        <div>
-            <Outlet />
-        </div>
-    </div>
+    const headerMenus = useHeaderMenus()
+
+    return <Layout className='h-screen'>
+        <Header>
+            <div className='float-left'>LOGO</div>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['content']} items={headerMenus} />
+        </Header>
+        <Layout>
+            <Sider width={200} className='shadow' theme='light'>
+                Menu
+            </Sider>
+            <Layout>
+                <Content>
+                    <Outlet />
+                </Content>
+            </Layout>
+        </Layout>
+    </Layout>
 }
 
 export default MainLayout
