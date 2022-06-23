1f501022afa78b336ce393d8267d08c41b3398ee
Chongyi <xpz3847878@163.com>
2022-06-23 18:13:40 +0800

    内容编辑

--- src/pages/Content/components/ContentEditor.js
+++ src/pages/Content/components/ContentEditor.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import MdEditor from 'md-editor-rt';
 import 'md-editor-rt/lib/style.css';
+import './style.css'
 
-const ContentEditor = ({ data }) => {
+const ContentEditor = ({ data, ...props }) => {
     const [content, setContent] = useState(data)
     return <MdEditor
         modelValue={content}
         onChange={setContent}
+        {...props}
     />
 }
 
